ea90f02e57559464576014417bd5a810d91df2fe
Cristian Diaz <diazcristian0210@gmail.com>
2024-04-29 22:40:25 -0300

    Agregue el ejercicio 6 al tp de react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CardColor from "./components/CardColor";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='py-4'>
+        <CardColor></CardColor>
+      </div>
+    </>
+  )
+}
+
+export default App
